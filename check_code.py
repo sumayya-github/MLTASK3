6f459afc7950b1df054b29147c144de5962806ff
sumayya-github <66043790+sumayya-github@users.noreply.github.com>
2020-06-27 07:40:36 +0530

    CHECK_CODE first commit

---
+++ check_code.py
@@ -0,0 +1,10 @@
+programfile= open("/program.py",'r')
+code = programfile.read()
+
+if 'keras' or 'tensorflow' in code:
+       if 'Conv2D' in code:
+            print("ALREADY EXISTS")
+       else:
+            print("NOT A CNN")
+else:
+     print("NOT A NEURAL NETWORK")
