362bbebbdef6c58f4c9d4ba1d12e1fed79d554f0
sumayya-github <66043790+sumayya-github@users.noreply.github.com>
2020-06-28 19:52:56 +0530

    mail first commit

--- mail.py
+++ mail.py
@@ -6,6 +6,6 @@ s = login("sumayyakhatoon58@gmail.com", "summaiya@26")
 
 message = "YOUR ACCURACY IS LESS THAN 90%.TRY AGAIN"
 
-s.sendmail("sumayyakhatoon58@gmail.com", message)
+s.sendmail("sumayyakhatoon58@gmail.com","s8355827840@gmail.com", message)
 
 s.quit()
